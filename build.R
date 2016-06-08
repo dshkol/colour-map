618ba48d27def9a603f7dbad0f45459875da71f3
dshkol <shkolnikd@gmail.com>
2016-06-07 23:55:04 -0700

    update build.R, added some plots for now.

--- build.R
+++ build.R
@@ -171,3 +171,70 @@ names(pal200df) <- c("pal1","pal3","pal5")
 # pal200df$pal5 <- as.character(pal200df$pal5)
 # fdat_van200 <- cbind.data.frame(fdat_van200,pal200df)
 # write_csv(fdat_van200, "fdat_van200.csv")
+
+
+
+# Plot with base graphics
+#plot(fdat_van200$lon, fdat_van200$lat, col = fdat_van200$pal1, pch = 19, cex = 2)
+
+# Get second most prominent colour from pal3
+# library(stringr)
+#fdat_van200$pal3_2 <- str_extract(fdat_van200$pal3, "\\,(.*)\\,")
+#fdat_van200$pal3_2 <- str_extract(fdat_van200$pal3, "#......")
+
+
+fdat_van25k <- filter(fdat_van) %>% sample_n(.,25000)
+
+start_time <- Sys.time()
+pal25k <- lapply(fdat_van25k$url, get_pal_safe)
+list_length_index <- lengths(pal25k)
+pal25kdf <- as.data.frame(do.call(rbind,lapply(pal25k, 'length<-',max(list_length_index))))
+names(pal25kdf) <- c("pal1","pal3","pal5")
+end_time <- Sys.time()
+
+van25 <- cbind.data.frame(fdat_van25k,pal25kdf)
+van25_bak <- van25
+van25$pal1 <- as.character(van25$pal1)
+van25$pal3 <- as.character(van25$pal3)
+van25$pal5 <- as.character(van25$pal5)
+
+
+
+# Generate colour matrix
+
+colmat <- matrix(van25$pal1, nrow = 125, ncol = 200)
+matpos <- as.data.frame(which(colmat != 0, arr.ind = TRUE))
+matpos$pal <- van25$pal1
+
+# Sort colours by converting into HSV
+require(colorspace)
+
+# 2 step process:
+# 1: convert hex to RGB
+# 2: convert RGB to HSV
+
+safe_hex2RGB <- failwith(NA, hex2RGB)
+safe_rgb2hsv <- failwith(NA, rgb2hsv)
+
+get_hsv <- function(x) {
+  tmp <- safe_hex2RGB(x)
+  hsv <- safe_rgb2hsv(matrix(tmp@coords, nrow = 3, ncol =1))
+  hsv <- t(hsv)
+  return(hsv)
+}
+
+get_hsv <- failwith(NA,get_hsv)
+# get HSV values
+pal25hsl <- sapply(pal25kdf$pal1, get_hsv)
+
+# convert into DF
+palhsl <- list()
+palhsl$h <- sapply(pal25hsl, "[[",1)
+palhsl$s <- sapply(pal25hsl, "[[",2)
+palhsl$l <- sapply(pal25hsl, "[[",3)
+palhsl <- as.data.frame(palhsl)
+
+# generate PCA
+require(FactoMineR)
+
+
